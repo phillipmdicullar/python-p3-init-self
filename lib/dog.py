6711b0af60e401280e3943e89039c267940db7fc
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-11-28 19:34:56 +0300

    created instances and called the methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+        
+    def bark(self):
+        print("Woof!")
+    
+    def get_adopted(self, owner_name):
+        self.owner_name = owner_name   
     pass
+
+my_dog = Dog(name="Dog")
+print(f"My dog name is {my_dog.name} and the breed is {my_dog.breed}")
+my_dog.bark()
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name="Person"):
+        self.name = name
     pass
+person_instance = Person()
+print(person_instance.name)
